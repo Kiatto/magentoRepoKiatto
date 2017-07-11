9ea535bdba8ef6394ad67d0a0ac8762cb0783e8b
Kiatto <luigikiatto@gmail.com>
2017-07-11 09:41:23 +0200

    magento2

---
+++ app/code/Kiatto/KiattoModule/Block/Prova.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Kiatto\KiattoModule\Block;
+
+class Prova extends \Magento\Framework\View\Element\Template
+{
+    private $_productCollectionFactory;
+
+   public function __construct
+   (
+       \Magento\Framework\View\Element\Template\Context $context,
+       \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory,
+       array $data
+   )
+   {
+       parent::__construct($context, $data);
+
+       $this->_productCollectionFactory = $productCollectionFactory;
+   }
+
+   public function getProducts()
+   {
+       $collection = $this->_productCollectionFactory->create();
+
+       $collection
+            ->addAttributeToSelect('*')
+            ->setOrder('created_at')
+            ->setPageSize(5);
+
+       return $collection;
+    }
+    
+}
